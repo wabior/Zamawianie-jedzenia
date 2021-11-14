bc2a8593b6130d44b71694f615f5c5f22adf0a67
wabior <wabior@gmail.com>
2021-11-14 06:49:11 +0100

    firestore conf works

---
+++ db.js
@@ -0,0 +1,8 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+  import firebaseConfig from './env.js';
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+const app = initializeApp(firebaseConfig);
+
+console.log(firebaseConfig);
