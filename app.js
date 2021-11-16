d897e27eaa1e52f1787bda5c39df4c69774bc1cb
wabior <wabior@gmail.com>
2021-11-16 06:35:10 +0100

    zwraca wyniki do widoku

--- app.js
+++ app.js
@@ -9,40 +9,25 @@ const warning = document.querySelector('#warning');
 const spanPerson = document.createElement('span');
 const spanOrder = document.createElement('span');
 
-const checkDuplicate = function(list){
-    for (let item of list) {
-        if (item.innerText === li.innerText){
-            console.log('Istnieje identyczne zamówiene.');
-            warning.innerText = 'Istnieje identyczne zamówiene.';
-
-            return false;
-        }
-    }
-    console.log('brak duplikatu')
-    warning.innerText = '';
-    
-return true;
-} 
-
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    spanPerson.innerText = `${person.value}`.trim();
     let personName = `${person.value}`.trim();
-    spanPerson.classList.add('person-submitted');
-
-    spanOrder.innerText = `${order.value}`.trim();
-    spanOrder.classList.add('order-submitted');
     let orderName = `${order.value}`.trim();
     let fullOrder = new Object;
+
+    spanPerson.innerText = `${person.value}`.trim();
+    spanPerson.classList.add('person-submitted');
+    spanOrder.innerText = `${order.value}`.trim();
+    spanOrder.classList.add('order-submitted');
+    
     fullOrder[personName] = orderName;
 
     li.appendChild(spanPerson);
     li.appendChild(spanOrder);
-
-    let list = document.querySelectorAll('li');  
   
-    addOrder(fullOrder);
+    wynik = addOrder(fullOrder);
     orderList.appendChild(li);
+    console.log(wynik);
      
 })
--- db.js
+++ db.js
@@ -19,22 +19,29 @@ export async function addOrder(order){
     try {  
         const docRef = await addDoc(collection(db, "orders"), order);
         console.log("Document written with ID: ", docRef.id);
+        return getAllOrders();
     } catch (e) {
         console.error("Error adding document: ", e);
     }
 }
 
 async function getAllOrders(){
+    let dupa = [];
     allOrders.forEach(element => {
+        dupa.push(element);
         console.log(JSON.stringify(element.data()));
     });
+    return dupa;
 }
 
 const q = query(collection(db, "orders"));
 onSnapshot(q, (querySnapshot) => {
+    let ordersFromDb = [];
     console.clear();
     querySnapshot.forEach((doc) => {
-    console.log(doc.data());
+        ordersFromDb.push(doc.data());
+        console.log(doc.data());
   });
+  console.log(ordersFromDb);
 });
 
