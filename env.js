952712ad28bd93aeda2cee6de4a999a5a8ea90a3
wabior <wabior@gmail.com>
2021-11-16 20:23:11 +0100

    style

---
+++ env.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCDklgBZGfGYmjMo2a_yKnzDWinBu7D6nw",
+
+  authDomain: "zamawianie-jedzenia.firebaseapp.com",
+
+  projectId: "zamawianie-jedzenia",
+
+  storageBucket: "zamawianie-jedzenia.appspot.com",
+
+  messagingSenderId: "598244322142",
+
+  appId: "1:598244322142:web:2c4eea25294352a6bf7dcf"
+
+};
+
+export default { firebaseConfig };
